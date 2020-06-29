d9b0cd3e8f306461dfd502274bfe30c8792c9dcd
Ajmat5392 <66867782+Ajmat5392@users.noreply.github.com>
2020-06-29 11:47:24 +0530

    Add files via upload

---
+++ BottomHalf/EcommerceDemo/src/test/java/org/bottomhalf/ec/controller/EcommerceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bottomhalf.ec.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
